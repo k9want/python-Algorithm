618ccfbef508450090caf0a9de6d2b14ab6e0459
qma <zndwlrnjr26@daum.net>
2022-01-23 02:31:06 +0900

    DFS/BFS example

--- DFS_BFS/AdjacencyList.py
+++ DFS_BFS/AdjacencyList.py
@@ -3,8 +3,7 @@ graph = [[] for _ in range(3)]
 
 graph[0].append((1, 7))
 graph[0].append((2, 5))
-graph[1].append((0,7))
-graph[2].append((1,5))
+graph[1].append((0, 7))
+graph[2].append((1, 5))
 
 print(graph)
-
