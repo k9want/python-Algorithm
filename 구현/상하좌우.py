f5f9a8aafffe4d3e8ecb1d8ff469504f0603007f
qma <zndwlrnjr26@daum.net>
2022-04-27 21:11:05 +0900

    구현 - 연습문제 상하좌우.py

---
+++ 구현/상하좌우.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+x = 1
+y = 1
+
+data = input().split()
+print(data)
+dy = [0,0,-1,1]
+dx = [-1,1,0,0]
+moves = ['L','R','U','D']
+
+for d in data:
+    for i in range(4):
+        if moves[i] == d:
+            ny = dy[i] + y
+            nx = dx[i] + x
+
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x = nx
+    y = ny
+
+print(x, y)
+
