889703d505c3a453605e426c0c55517f073207a7
qma <zndwlrnjr26@daum.net>
2022-05-06 00:37:01 +0900

    feat: Add 가사검색-(프로그래머스).py

---
+++ 이진탐색/가사검색-(프로그래머스).py
@@ -0,0 +1,42 @@
+"""
+1. 이진탐색은 반드시 정렬부터!!
+2.array와 reverse_array로 나눠서 각각 찾기
+3.아이디어 ?를 a와 z로 바꾸기
+"""
+
+from bisect import bisect_left, bisect_right
+
+
+def count_by_range(array, left_value, right_value):
+    left_index = bisect_left(array, left_value)
+    right_index = bisect_right(array, right_value)
+    return right_index - left_index
+
+
+array = [[] for _ in range(10001)]
+
+reverse_array = [[] for _ in range(10001)]
+
+
+def solution(words, queries):
+    answer = []
+
+    for word in words:
+        array[len(word)].append(word)
+        reverse_array[len(word)].append(word[::-1])
+
+    for i in range(10001):
+        array[i].sort()
+        reverse_array[i].sort()
+
+    for q in queries:
+        if q[0] != '?':
+            res = count_by_range(array[len(q)], q.replace('?', 'a'), q.replace('?', 'z'))
+        else:
+            res = count_by_range(reverse_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
+
+        answer.append(res)
+    return answer
+
+
+
