039e8c81b3ca1e527e9cf2e6adc6c0cd19773ad9
qma <zndwlrnjr26@daum.net>
2022-05-24 14:29:38 +0900

    feat: Add 정수 삼각형.py

---
+++ 프로그래머스/코딩테스트 고득점 Kit/동적계획법/정수 삼각형.py
@@ -0,0 +1,19 @@
+"""
+문제에 나온 점화식을 그대로 코드로 작성하면된다.
+"""
+
+
+def solution(triangle):
+    answer = 0
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i - 1][j]
+            elif j == len(triangle[i]) - 1:
+                triangle[i][j] += triangle[i - 1][j - 1]
+            else:
+                triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
+
+    # print(triangle)
+
+    return max(triangle[-1])
