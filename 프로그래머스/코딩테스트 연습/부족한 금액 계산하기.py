b0628f59775490b303b9ab34c835698cc70491b1
qma <zndwlrnjr26@daum.net>
2022-06-21 19:11:41 +0900

    feat: Add 부족한 금액 계산하기.py

---
+++ 프로그래머스/코딩테스트 연습/부족한 금액 계산하기.py
@@ -0,0 +1,12 @@
+def solution(price, money, count):
+    answer = 0
+    result = 0
+    for i in range(1, count + 1):
+        result += (price * i)
+
+    if result >= money:
+        answer = result - money
+    elif result <= money:
+        answer = 0
+
+    return answer
