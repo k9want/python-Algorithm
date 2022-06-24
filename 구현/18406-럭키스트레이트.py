5ce00b05ba5cc09c3cc1fa59c080d1493f1172c1
qma <zndwlrnjr26@daum.net>
2022-06-24 23:47:27 +0900

    docs: Modified 18406-럭키스트레이트.py

--- 구현/18406-럭키스트레이트.py
+++ 구현/18406-럭키스트레이트.py
@@ -1,16 +1,37 @@
-n = list(map(int, input()))
+# n = list(map(int, input()))
+#
+# middle = len(n)//2
+#
+# left = 0
+#
+# right = 0
+# for i in range(middle):
+#     left += n[i]
+#
+#
+# for i in range(middle, len(n)):
+#     right += n[i]
+#
+# if left == right:
+#     print("LUCKY")
+# else:
+#     print("READY")
 
-middle = len(n)//2
 
+n = input()
+
+length = len(n)
+mid = length // 2
+cnt = 0
 left = 0
-
 right = 0
-for i in range(middle):
-    left += n[i]
 
-
-for i in range(middle, len(n)):
-    right += n[i]
+for s in n:
+    if cnt < mid:
+        right += int(s)
+    else:
+        left += int(s)
+    cnt += 1
 
 if left == right:
     print("LUCKY")
