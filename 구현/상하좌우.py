aa0527c0a1cb148e036fba3893d1668c25443df4
qma <zndwlrnjr26@daum.net>
2022-06-24 21:34:39 +0900

    docs: Modified 무지의 먹방 라이브.py

--- 구현/상하좌우.py
+++ 구현/상하좌우.py
@@ -4,21 +4,42 @@ x = 1
 y = 1
 
 data = input().split()
-print(data)
+# print(data)
 dy = [0,0,-1,1]
 dx = [-1,1,0,0]
 moves = ['L','R','U','D']
 
+
 for d in data:
     for i in range(4):
         if moves[i] == d:
-            ny = dy[i] + y
-            nx = dx[i] + x
+            ny = y + dy[i]
+            nx = x + dx[i]
 
-    if nx < 1 or ny < 1 or nx > n or ny > n:
-        continue
-    x = nx
-    y = ny
+    if 0 < ny <= n and 0 < nx <= n:
+        x = nx
+        y = ny
 
-print(x, y)
+print(y, x)
 
+
+
+
+
+# for d in data:
+#     for i in range(4):
+#         if moves[i] == d:
+#             ny = dy[i] + y
+#             nx = dx[i] + x
+#
+#     if nx < 1 or ny < 1 or nx > n or ny > n:
+#         continue
+#     x = nx
+#     y = ny
+#
+# print(x, y)
+
+"""
+5
+R R R U D D
+"""
