cafacfe7716c1a331ece02d05d36249f605c8578
qma <zndwlrnjr26@daum.net>
2022-06-28 23:29:40 +0900

    feat: Add 줄서는방법.py

---
+++ 프로그래머스/코딩테스트 연습(Lv3)/줄서는방법.py
@@ -0,0 +1,20 @@
+import math
+
+
+def solution(n, k):
+    answer = [i for i in range(1, n + 1)]
+    stk = []
+    k = k -1
+
+    while answer:
+        # k / (n-1)! 을 했을 때의 몫이 맨 첫번째 자리
+        a = k // math.factorial(n - 1)
+        stk.append(answer[a])
+        del answer[a]
+
+        # k를 줄여야함.
+        k = k % math.factorial(n - 1)
+        n -= 1
+
+
+    return stk
