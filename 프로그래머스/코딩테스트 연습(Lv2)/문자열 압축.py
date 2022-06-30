6c24bf96b8ba28fb9e31818aa678ebee35f22498
qma <zndwlrnjr26@daum.net>
2022-06-30 20:50:37 +0900

    docs: Update 문자열 압축.py

--- 프로그래머스/코딩테스트 연습(Lv2)/문자열 압축.py
+++ 프로그래머스/코딩테스트 연습(Lv2)/문자열 압축.py
@@ -33,3 +33,27 @@ def solution(s):
         answer = min(answer, len(result))
 
     return answer
+
+
+"""
+def solution(s):
+    answer = len(s)
+    
+    for step in range(1, len(s)//2+1):
+        comp = ''
+        cnt = 1
+        prev = s[0:step]
+        for j in range(step, len(s), step):
+            if prev == s[j:j+step]:
+                cnt += 1
+            else:
+                comp += str(cnt) + prev if cnt >= 2 else prev
+                cnt = 1
+                prev = s[j:j+step]
+            
+        comp += str(cnt) + prev if cnt >= 2 else prev
+        answer = min(answer, len(comp))
+    
+    
+    return answer
+"""
